b40106aadc953c257087c344c58567734b9a4808
Mirek <nyziu@DESKTOP-N9P8O81>
2023-01-15 01:23:38 -0500

    added welcome screen

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,72 +1,132 @@
 // Declare array of objects/questions
-var questions = [
+const questionsArray = [
     {
         question: "Arrays in JavaScript can be used to store _____.",
-        option1: "1. numbers and strings",
-        option2: "2. other arrays",
-        option3: "3. booleans",
-        option4: "4. all of the above",
-        answer: "all of the above"
+        optionA: "a. numbers and strings",
+        optionB: "b. other arrays",
+        optionC: "c. booleans",
+        optionD: "d. all of the above",
+        answer: "d"
     },
     {
         question: "Loos in JS:",
-        option1: "Option5",
-        option2: "Option6",
-        option3: "Option7",
-        option4: "Option8",
-        answer: "Option1"
+        optionA: "Option5",
+        optionB: "Option6",
+        optionC: "Option7",
+        optionD: "Option8",
+        answer: "a"
     } 
 ]
+
+
 // Creating DOM Elements
+
+
+// Quiz welcome/start screen
+var startHeader = document.createElement('h2');
+startHeader.setAttribute("class", "center");
+startHeader.textContent = "Coding Quiz Challenge";
+var startMessage1 = document.createElement('p');
+startMessage1.setAttribute("class", "center start");
+startMessage1.textContent = "Try to answer the following code-related questions within the time limit."
+var startMessage2 = document.createElement('p');
+startMessage2.setAttribute("class", "center start");
+startMessage2.textContent = "Keep in mind that incorrect answers will penalize your score/time by ten seconds!"
+var startQuizButton = document.createElement("button");
+startQuizButton.setAttribute("class", "button start");
+startQuizButton.textContent = "Start Quiz";
+
+
+
+// Quiz question container
 var quizContainer = document.getElementById('quiz-container');
 // Question number
 var quizQuestionNumber = document.createElement('h4');
 // Question title
-var quizTitle = document.createElement('h2');
+var quizQuestion = document.createElement('h2');
+var quizListContainer = document.createElement('div');
+quizListContainer.setAttribute("id", "list-container");
 var quizUl = document.createElement('ul');
 // Answers as list items
 var quizli1 = document.createElement('li')
+quizli1.setAttribute("class", "question")
 var quizli2 = document.createElement('li');
+quizli2.setAttribute("class", "question")
 var quizli3 = document.createElement('li');
+quizli3.setAttribute("class", "question")
 var quizli4 = document.createElement('li');
+quizli4.setAttribute("class", "question")
 // Display correct / wrong answer
 var quizAnswerContainer = document.createElement('div')
     // Set it's initial state to hidden
-    // quizAnswerContainer.setAttribute("data-state", "hidden")
+    quizAnswerContainer.setAttribute("class", "answerContainer")
 var timerAnswer = document.createElement('div')
 timerAnswer.setAttribute("class", "timer");
 
 
-// quizTitle.setAttribute("class", "answer")
 
-var currentQuestionIndex = 0;
-var remainingTime = 90;
+
+
+// quizQuestion.setAttribute("class", "answer")
+
+var currentQuestionIndex;
+var remainingTime;
+
+function startQuiz() {
+    // Reset global values
+    currentQuestionIndex = 0;
+    remainingTime = 90;
+    showWelcomeScreen();
+}
 
 function resetQuiz() {
     currentQuestionIndex = 0;
 }
 
-// function currentQuestion (questionArray, questionIndex) {
-function currentQuestion () {
+function showWelcomeScreen() {
+    quizContainer.appendChild(startHeader);
+    quizContainer.appendChild(startMessage1);
+    quizContainer.appendChild(startMessage2);
+    quizContainer.appendChild(startQuizButton);    
+}
 
-    if (questions[currentQuestionIndex] != undefined) {
+function removeWelcomeScreen() {
+    quizContainer.removeChild(startHeader);
+    quizContainer.removeChild(startMessage1);
+    quizContainer.removeChild(startMessage2);
+    quizContainer.removeChild(startQuizButton);    
+    currentQuestion();
+}
+
+
+
+
+// function currentQuestion (questionArray, questionIndex) {
+function currentQuestion () {    
+
+    if (questionsArray[currentQuestionIndex] != undefined) {
+        
+        if (currentQuestionIndex === 0){
+            quizContainer.appendChild(quizQuestionNumber)
+            quizContainer.appendChild(quizQuestion);
+            quizContainer.appendChild(quizListContainer)
+            quizListContainer.appendChild(quizUl);
+            quizUl.appendChild(quizli1);
+            quizUl.appendChild(quizli2);
+            quizUl.appendChild(quizli3);
+            quizUl.appendChild(quizli4);
+        }
         
         // Update content based on object in the array
-        quizQuestionNumber.textContent =  "Question " + (currentQuestionIndex + 1) + "/" + questions.length;
-        quizTitle.textContent = questions[currentQuestionIndex].question;
-        quizli1.textContent = questions[currentQuestionIndex].option1;
-        quizli2.textContent = questions[currentQuestionIndex].option2;
-        quizli3.textContent = questions[currentQuestionIndex].option3;
-        quizli4.textContent = questions[currentQuestionIndex].option4;       
+        quizQuestionNumber.textContent =  "Question " + (currentQuestionIndex + 1) + "/" + questionsArray.length;
+        quizQuestion.textContent = questionsArray[currentQuestionIndex].question;
+        quizli1.textContent = questionsArray[currentQuestionIndex].optionA;
+        quizli2.textContent = questionsArray[currentQuestionIndex].optionB;
+        quizli3.textContent = questionsArray[currentQuestionIndex].optionC;
+        quizli4.textContent = questionsArray[currentQuestionIndex].optionD;       
         
-
-        quizContainer.appendChild(quizQuestionNumber)
-        quizContainer.appendChild(quizTitle);
-        quizContainer.appendChild(quizUl);
-        quizUl.appendChild(quizli1);
-        quizUl.appendChild(quizli2);
-        quizUl.appendChild(quizli3);
-        quizUl.appendChild(quizli4);
+        
+        
     }
     else {
         // Provide summary
@@ -75,17 +135,31 @@ function currentQuestion () {
     }
 }
 
-
 function submitAnswer(event) {
     var element = event.target;
     if (element.matches('li')) {
         
         var displayAnswerTime = 4;
 
+
+
+
+
+
+        if (element.textContent[0] === questionsArray[currentQuestionIndex].answer) {
+            console.log("Correct answer")
+            element.setAttribute("class", "correct disabled")
+        } else {
+            console.log("Incorrect answer")
+            element.setAttribute("class", "incorrect disabled")
+        }
+
+
         quizAnswerContainer.textContent = "Correct";
         timerAnswer.textContent = displayAnswerTime + 1;
         quizContainer.appendChild(quizAnswerContainer);
         quizContainer.append(timerAnswer);
+        
 
         // Display a box with with correct or wrong status for 5 sec
         var timerInterval = setInterval(function() {
@@ -100,7 +174,7 @@ function submitAnswer(event) {
                 
                 console.log("Success")
                 currentQuestionIndex++;
-                currentQuestion(questions, currentQuestionIndex);           
+                currentQuestion(questionsArray, currentQuestionIndex);           
             }
         }, 1000);   
 
@@ -109,11 +183,21 @@ function submitAnswer(event) {
         
         
         
-    }    
+    } else {
+        console.log("Not an li element")
+    }
+
 }
 
 
-currentQuestion();
 
-quizContainer.addEventListener("click", submitAnswer);
 
+
+
+// Event listeners
+quizUl.addEventListener("click", submitAnswer);
+startQuizButton.addEventListener("click", removeWelcomeScreen)
+
+// Display welcome screen
+// showWelcomeScreen()
+startQuiz()
