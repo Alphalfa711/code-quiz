1cf49998ffcc8cf14065aae22d6e312b304f92da
Mirek <nyziu@DESKTOP-N9P8O81>
2023-01-19 23:16:49 -0500

    Moving array with questions to seperate file

---
+++ assets/js/questions.js
@@ -0,0 +1,53 @@
+// Declare array of objects/questions
+const questionsArray = [
+    {
+        question: "Arrays in JavaScript are defined by which of the following statements?",
+        answers: [
+            "a. It is an ordered list of values",
+            "b. It is an ordered list of objects",
+            "c. It is an ordered list of string",
+            "d. It is an ordered list of functions"
+        ],
+        questionAnswer: "a"        
+    },
+    {
+        question: "Which of the following is correct about JavaScript?",
+        answers: [
+            "a. JavaScript is an Object-Based language",
+            "b. JavaScript is Assembly-language",
+            "c. JavaScript is an Object-Oriented language",
+            "d. JavaScript is a High-level language"
+        ],
+        questionAnswer: "a"
+    },    
+    {
+        question: "Which of the following are not server-side Javascript objects?",
+        answers: [
+            "a. Date",
+            "b. FileUpload",
+            "c. Function",
+            "d. All of the above"
+        ],
+        questionAnswer: "d"
+    },
+    {
+        question: "How do we write comment in javascript?",
+        answers: [
+            "a. /* */",
+            "b. //",
+            "c. #",
+            "d. $$"
+        ],
+        questionAnswer: "b"
+    },    
+    {
+        question: "Why JavaScript Engine is needed?",
+        answers: [  
+            "a. Both Compiling & Interpreting the JavaScript",
+            "b. Parsing the javascript",
+            "c. Interpreting the JavaScript",
+            "d. Compiling the JavaScript",
+        ],
+        questionAnswer: "c"
+    }
+]
